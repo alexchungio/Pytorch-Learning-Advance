70d5b3b0cfac60b871d9e909f4d7a65d8304b7d8
alexchung <yonganzhong@outlook.com>
2021-01-17 13:06:33 +0800

    update transformer inference module

--- sentiment_analysis/inference_gru_transformer.py
+++ sentiment_analysis/inference_gru_transformer.py
@@ -20,14 +20,14 @@ from transformers import BertTokenizer, BertModel
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-MODEL_PATH = './output/best_transformer_model.pth'
+MODEL_PATH = './output/transformer_model_best.pth'
 
 nlp = spacy.load('en')
 
 state = torch.load(MODEL_PATH)
 HIDDEN_SIZE = state['hidden_size']
 OUTPUT_SIZE = state['output_size']
-NUM_LAYERS = state['num_layers']
+NUM_LAYERS = state['num_layer']
 BIDIRECTIONAL = state['bidirectional']
 DROPOUT = state['dropout']
 
@@ -35,14 +35,21 @@ DROPOUT = state['dropout']
 def predict(model, sentence, tokenizer):
 
     model.eval()
+    # step 1 tokenize
     tokens = tokenizer.tokenize(sentence)
+    # step 2 get max_length
+    max_input_length = tokenizer.max_model_input_sizes['bert-base-uncased']
+    # cut sentence for the following insert head and tail to the text
+    tokens = tokens[:max_input_length - 2]
+    # token to index
     token_index = tokenizer.convert_tokens_to_ids(tokens)
-
+    # add head and tail
+    token_index = [tokenizer.cls_token_id] + token_index + [tokenizer.sep_token_id]
     # (seq_length, )
     text_tensor = torch.LongTensor(token_index).to(device)
     # (seq_length) => (batch_size, seq_length) => (seq_length, batch_size)
     # text_tensor = text_tensor.unsqueeze(dim=0).transpose(1, 0)
-
+    text_tensor = text_tensor.unsqueeze(0)
     pred = torch.sigmoid(model(text_tensor))
 
     return pred
@@ -50,8 +57,7 @@ def predict(model, sentence, tokenizer):
 
 def main():
 
-    tokenizer = BertTokenizer('bert-base-uncased')
-
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
     bert_model = BertModel.from_pretrained('bert-base-uncased')
     model = BertGRUSentiment(bert=bert_model,
                              hidden_size=HIDDEN_SIZE,
@@ -66,8 +72,12 @@ def main():
 
     sentence_0 = 'This film is terrible'
     pred_0 = predict(model, sentence_0, tokenizer)
-    print(pred_0.item())  # 0.08206960558891296
+    print(pred_0.item()) # 0.031715769320726395
 
     sentence_1 = 'This film is great'
     pred_1 = predict(model, sentence_1, tokenizer)
-    print(pred_1.item())  # 0.8348122835159302
+    print(pred_1.item()) # 0.9611917734146118
+
+
+if __name__ == "__main__":
+    main()
